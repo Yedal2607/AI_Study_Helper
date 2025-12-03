0f9848e1a4a231aaad7c865ac8ccccbe93d0b11c
Yedal <abreuyedal@gmail.com>
2025-12-03 10:09:48 -0500

    feat: Adding water wave animation for the background

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        //Water wave animation for the background
+        water: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+      },
+      animation: {
+        water: 'water 20s ease-in-out infinite',
+      },
+    },    
   },
   plugins: [],
 };
