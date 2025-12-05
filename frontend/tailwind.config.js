b16653abf649fe85207cbe769d635f7ea3200dd7
Yedal <abreuyedal@gmail.com>
2025-12-05 12:13:38 -0500

    feat: Add inter font to tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,16 +3,9 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        //Water wave animation for the background
-        water: {
-          '0%, 100%': { backgroundPosition: '0% 50%' },
-          '50%': { backgroundPosition: '100% 50%' },
-        },
-      },
-      animation: {
-        water: 'water 20s ease-in-out infinite',
-      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"]
+      }
     },    
   },
   plugins: [],
