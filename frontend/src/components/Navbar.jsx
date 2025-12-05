6b531d501f4057cf2d8d55088fcba6668bb3492d
Yedal <abreuyedal@gmail.com>
2025-12-05 12:24:42 -0500

    style: update font and adjust layout

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <nav>
+    <nav className="w-[80%]">
 
-      {/*Center Nav elements*/}
-      <div >
-        <Link to="/" className="text-[#ffffff]">Home</Link>
+      <div className="flex h-[80px] items-center">
+        <Link to="/" className="text-[1.3em] text-[#ffffff] font-inter font-[700] transition duration-130 hover:scale-[1.3]">Home</Link>
       </div>
     </nav>
   )
