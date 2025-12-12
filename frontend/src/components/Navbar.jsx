419bf533b83be829edf7dc21735ce086a3555170
Yedal <abreuyedal@gmail.com>
2025-12-12 17:39:41 -0500

    feat: [200~feat: add FontAwesome and integrate icons in navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from '@fortawesome/free-solid-svg-icons'
 function Navbar() {
   return (
     <nav className="w-[80%]">
 
       <div className="flex h-[80px] items-center">
-        <Link to="/" className="text-[1.3em] text-[#ffffff] font-inter font-[700] transition duration-130 hover:scale-[1.3]">Home</Link>
+        <Link to="/" className="text-[1.5em] text-[#ffffff] font-inter font-[700] transition duration-130 hover:scale-[1.3]">
+          <FontAwesomeIcon icon={faHouse} />
+        </Link>
       </div>
     </nav>
   )
