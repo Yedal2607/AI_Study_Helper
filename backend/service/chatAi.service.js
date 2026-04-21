7644bce9b76ee7b2a80d9c7bc3ad12b7611db0a6
Yedal <abreuyedal@gmail.com>
2026-04-20 21:34:01 -0400

    fix: switch AI service provider and improve code structure

--- backend/service/chatAi.service.js
+++ backend/service/chatAi.service.js
@@ -1,29 +1,31 @@
-import { OpenAI } from "openai";
-
-const api = new OpenAI({
-  baseURL: "https://api.aimlapi.com/v1",
-  apiKey: process.env.AI_API_KEY,
-});
 
 async function AskAI(userPrompt) {
   try {
-    const response = await api.chat.completions.create({
-      model: "openai/gpt-5-2",
-      messages: [
-        {
-          role: "system",
-          content: "You are an AI assistant who knows everything.",
-        },
-        {
-          role: "user",
-          content: userPrompt,
-        },
-      ],
+    const response = await fetch(process.env.AI_URL_PROVIDER, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${process.env.AI_API_KEY}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        model: process.env.AI_MODEL,
+        messages: [
+          {
+            role: "system",
+            content: "You are AI Study Helper, an AI assistant",
+          },
+          {
+            role: "user",
+            content: userPrompt,
+          },
+        ],
+        temperature: 0.7,
+        max_tokens: 512,
+      }),
     });
-
-    const answer =
-      response?.choices?.[0]?.message?.content ??
-      "AI service returned an unexpected response";
+    const data = await response.json();
+    const answer = data?.choices?.[0]?.message?.content || data?.message;
+    ("AI service returned an unexpected response");
     return { message: answer };
   } catch (error) {
     console.error(error);
