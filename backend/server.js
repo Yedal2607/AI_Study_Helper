0883f5f404751315ff743232722f92b9a13f73e3
Yedal <abreuyedal@gmail.com>
2026-05-12 20:08:27 -0400

    fix: improve server.js code structure and add auth route

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,22 @@
 import "dotenv/config";
-import express from 'express';
+import express from "express";
 import cors from "cors";
-import chatAIRouter from "./routes/chatAI.routes.js"
+import chatAIRouter from "./routes/chatAI.routes.js";
+import auth from "./routes/auth.routes.js"
 
-
-const PORT = process.env.BACKEND_PORT;
 const app = express();
+const PORT = process.env.BACKEND_PORT;
 
-
+/* Middlewares */
 app.use(express.json());
 app.use(cors());
+
+/* Routes */
+app.use("/auth", auth)
 app.use("/chat", chatAIRouter);
+
+
+/* Server */
 app.listen(PORT, () => {
-
-    console.log(`Servidor corriendo en http://localhost:${PORT}`)
-    
-})
-
-    
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
