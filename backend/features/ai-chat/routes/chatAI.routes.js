f47b57d9322543299fd3d3003557cdeabdcb831a
Yedal2607 <abreuyedal@gmail.com>
2026-09-02 07:33:18 -0400

    feat(auth): add authorization middleware handling

--- backend/features/ai-chat/routes/chatAI.routes.js
+++ backend/features/ai-chat/routes/chatAI.routes.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import {chatAI} from "../controller/chatAI.controller.js"
+import { requireAuth } from "../../../middleware/auth.middleware.js";
 const router = Router();
 
-router.post("/ask",chatAI)
+router.post("/ask",requireAuth,chatAI)
 
 export default router;
